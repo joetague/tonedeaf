5dbcbbf05a16eb5b57ed0a125cd1221a73890521
Joe Tague <joetague@gmail.com>
2014-03-18 20:33:46 +0000

    Added overtone 0.9.1 dependency

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject tonedeaf "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Project for messing about with Supercollider and Overtone: definitely not going to be good music!"
+  :url "https://github.com/joetague/tonedeaf"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+  				 [overtone "0.9.1"]])
