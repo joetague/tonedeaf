7fb78713564b6651d74d8b8d331881ccd27d89a8
Joe Tague <joetague@gmail.com>
2014-03-18 22:17:34 +0000

    Added instrument defs from page https://github.com/overtone/overtone/wiki/Oscillators. Also created a newbeep instrument that doesnt crash with TOO LOUD when using the map later (see comment for map code as demo'd by Chris)

--- src/tonedeaf/core.clj
+++ src/tonedeaf/core.clj
@@ -1,6 +1,41 @@
 (ns tonedeaf.core)
+(use 'overtone.live)
 
-(definst beep [note 60] 
-  (let [src (sin-osc (midicps note)) 
-  env (env-perc 0.01 1.0 :action :free)] 
-  (* src env)))
+(definst sin-wave [freq 440 attack 0.01 sustain 0.4 release 0.1 vol 0.4] 
+  (* (env-gen (lin attack sustain release) 1 1 0 1 FREE)
+     (sin-osc freq)
+     vol))
+(sin-wave)
+(definst saw-wave [freq 440 attack 0.01 sustain 0.4 release 0.1 vol 0.4] 
+  (* (env-gen (lin attack sustain release) 1 1 0 1 FREE)
+     (saw freq)
+     vol))
+
+(definst square-wave [freq 440 attack 0.01 sustain 0.4 release 0.1 vol 0.4] 
+  (* (env-gen (lin attack sustain release) 1 1 0 1 FREE)
+     (lf-pulse:ar freq)
+     vol))
+
+(definst noisey [freq 440 attack 0.01 sustain 0.4 release 0.1 vol 0.4] 
+  (* (env-gen (lin attack sustain release) 1 1 0 1 FREE)
+     (pink-noise) ; also have (white-noise) and others...
+     vol))
+
+(definst triangle-wave [freq 440 attack 0.01 sustain 0.1 release 0.4 vol 0.4] 
+  (* (env-gen (lin attack sustain release) 1 1 0 1 FREE)
+     (lf-tri freq)
+     vol))
+
+(definst spooky-house [freq 440 width 0.2 
+                         attack 0.3 sustain 4 release 0.3 
+                         vol 0.4] 
+  (* (env-gen (lin attack sustain release) 1 1 0 1 FREE)
+     (sin-osc (+ freq (* 20 (lf-pulse:kr 0.5 0 width))))
+     vol))
+
+(definst newbeep [note 60 attack 0.01 sustain 0.8 release 0.4 vol 0.4] 
+  (* (sin-osc (midicps note)) 
+     (env-gen (env-perc attack sustain) 1 1 0 1 FREE)
+     ))
+
+;;(map #(at (+ (now) (* % 20)) (beep %)) (range 110))
