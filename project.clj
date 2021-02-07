a91df2104e3a4270c6e16326d368e5cb943aa10f
Joe Tague <joetague@users.noreply.github.com>
2021-02-07 15:43:23 +0000

    Use Clojure CLI

--- project.clj
+++
@@ -1,7 +0,0 @@
-(defproject tonedeaf "0.1.0-SNAPSHOT"
-  :description "Project for messing about with Supercollider and Overtone: definitely not going to be good music!"
-  :url "https://github.com/joetague/tonedeaf"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-  				 [overtone "0.10.6"]])
--- src/tonedeaf/core.clj
+++ src/tonedeaf/core.clj
@@ -1,40 +1,39 @@
-(ns tonedeaf.core)
+(ns tonedeaf.core
+  (:require [overtone.core :as o]))
 
-(use 'overtone.core)
-
-(definst sin-wave [freq 440 attack 0.01 sustain 0.4 release 0.1 vol 0.4]
+(o/definst sin-wave [freq 440 attack 0.01 sustain 0.4 release 0.1 vol 0.4]
   (* (env-gen (lin attack sustain release) 1 1 0 1 FREE)
      (sin-osc freq)
      vol))
 (sin-wave)
-(definst saw-wave [freq 440 attack 0.01 sustain 0.4 release 0.1 vol 0.4]
+(o/definst saw-wave [freq 440 attack 0.01 sustain 0.4 release 0.1 vol 0.4]
   (* (env-gen (lin attack sustain release) 1 1 0 1 FREE)
      (saw freq)
      vol))
 (saw-wave)
-(definst square-wave [freq 440 attack 0.01 sustain 0.4 release 0.1 vol 0.4]
+(o/definst square-wave [freq 440 attack 0.01 sustain 0.4 release 0.1 vol 0.4]
   (* (env-gen (lin attack sustain release) 1 1 0 1 FREE)
      (lf-pulse:ar freq)
      vol))
 (square-wave)
-(definst noisey [freq 440 attack 0.01 sustain 0.4 release 0.1 vol 0.4]
+(o/definst noisey [freq 440 attack 0.01 sustain 0.4 release 0.1 vol 0.4]
   (* (env-gen (lin attack sustain release) 1 1 0 1 FREE)
      (pink-noise) ; also have (white-noise) and others...
      vol))
 (noisey)
-(definst triangle-wave [freq 440 attack 0.01 sustain 0.1 release 0.4 vol 0.4]
+(o/definst triangle-wave [freq 440 attack 0.01 sustain 0.1 release 0.4 vol 0.4]
   (* (env-gen (lin attack sustain release) 1 1 0 1 FREE)
      (lf-tri freq)
      vol))
 (triangle-wave)
-(definst spooky-house [freq 440 width 0.2
-                       attack 0.3 sustain 4 release 0.3
-                       vol 0.4]
+(o/definst spooky-house [freq 440 width 0.2
+                         attack 0.3 sustain 4 release 0.3
+                         vol 0.4]
   (* (env-gen (lin attack sustain release) 1 1 0 1 FREE)
      (sin-osc (+ freq (* 20 (lf-pulse:kr 0.5 0 width))))
      vol))
 (spooky-house)
-(definst newbeep [note 60 attack 0.01 sustain 0.8 release 0.4 vol 0.4]
+(o/definst newbeep [note 60 attack 0.01 sustain 0.8 release 0.4 vol 0.4]
   (* (sin-osc (midicps note))
      (env-gen (env-perc attack sustain) 1 1 0 1 FREE)))
 (newbeep)
